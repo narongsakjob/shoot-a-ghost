9a63681c610a261823596bf4e3b7a2e924991305
Narongsak Chobsri <Job@Narongsaks-MacBook-Pro.local>
2016-03-29 12:04:26 +0700

    123

---
+++ src/Bullet.js
@@ -0,0 +1,9 @@
+var Bullet = cc.Sprite.extend({
+  ctor: function(){
+    this._super();
+    this.initWithFile('res/images/bullet.png');
+  },
+  update: function(dt){
+    this.setPosition( new cc.Point( screenWidth/2, this.getPositionY() + 50 ) );
+  }
+});
--- src/GameLayer.js
+++ src/GameLayer.js
@@ -5,8 +5,34 @@ var GameLayer = cc.LayerColor.extend({
         this.cannon = new Cannon();
         this.cannon.setPosition(new cc.Point(screenWidth/2,30));
         this.addChild(this.cannon);
+        this.addKeyboardHandlers();
+        // this.bullet = null;
+        this.scheduleUpdate();
+
         return true;
-    }
+    },
+    onKeyDown: function( keyCode, event ) {
+      this.bullet = new Bullet();
+      this.addChild(this.bullet);
+      this.bullet.setPosition(new cc.Point(screenWidth/2,30));
+      this.bullet.scheduleUpdate();
+      this.scheduleUpdate();
+    },
+    onKeyUp: function( keyCode, event ) {
+
+    },
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( keyCode, event ) {
+                self.onKeyDown( keyCode, event );
+            },
+            onKeyReleased: function( keyCode, event ) {
+                self.onKeyUp( keyCode, event );
+            }
+        }, this);
+    },
 });
 
 var StartScene = cc.Scene.extend({
