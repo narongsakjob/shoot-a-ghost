f7ea67b648a5a0889b94ea60d690d5eb56c254cf
Narongsak Chobsri <Job@Narongsaks-MacBook-Pro.local>
2016-04-01 16:14:53 +0700

    check 1 shot

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,9 +1,17 @@
 var Bullet = cc.Sprite.extend({
+
   ctor: function(){
     this._super();
     this.initWithFile('res/images/bullet.png');
   },
   update: function(dt){
-    this.setPosition( new cc.Point( screenWidth/2, this.getPositionY() + 50 ) );
-  }
+
+    this.setPosition( new cc.Point( screenWidth/2, this.getPositionY() + 10 ) );
+
+
+  },
+
+
+
+
 });
--- src/GameLayer.js
+++ src/GameLayer.js
@@ -7,16 +7,37 @@ var GameLayer = cc.LayerColor.extend({
         this.addChild(this.cannon);
         this.addKeyboardHandlers();
         // this.bullet = null;
+        this.bullet = new Bullet();
+         this.bullet.setPosition(0,0);
         this.scheduleUpdate();
 
         return true;
     },
+    update : function(){
+      this.checkBullet();
+    },
     onKeyDown: function( keyCode, event ) {
-      this.bullet = new Bullet();
-      this.addChild(this.bullet);
-      this.bullet.setPosition(new cc.Point(screenWidth/2,30));
-      this.bullet.scheduleUpdate();
-      //this.scheduleUpdate();
+      if(keyCode == cc.KEY.space){
+        if(check==true){
+         this.bullet = new Bullet();
+         this.addChild(this.bullet);
+         this.bullet.scheduleUpdate();
+       }
+       }
+    //   this.bullet.setPosition(new cc.Point(screenWidth/2,30));
+
+        //this.scheduleUpdate();
+
+
+
+    },
+    checkBullet: function(){
+      if(this.bullet.getPositionY() > 600 || this.bullet.getPositionY()==0){
+        check =true;
+      }else{
+        check = false;
+      }
+
     },
     onKeyUp: function( keyCode, event ) {
 
@@ -43,3 +64,4 @@ var StartScene = cc.Scene.extend({
         this.addChild( layer );
     }
 });
+var check =true;
