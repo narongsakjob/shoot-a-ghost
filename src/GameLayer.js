b890b582a5a2aab27e6e1e62d21db497859612bf
Narongsak Chobsri <Job@Narongsaks-MacBook-Pro.local>
2016-04-18 18:05:02 +0700

    clean code

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,104 +2,106 @@ var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
+
+        this.addKeyboardHandlers();
+
         this.cannon = new Cannon();
         this.cannon.setPosition(new cc.Point(screenWidth/2,30));
         this.addChild(this.cannon);
-        this.addKeyboardHandlers();
+
         this.bullet = new Bullet();
-        this.bullet.setPosition(0,0);
+        this.bullet.setPosition(0,700);
         this.bullet.scheduleUpdate();
+
         this.checker = new Checker();
         this.checker.setPosition(screenWidth-50,190);
         this.addChild(this.checker);
+
         ghostArray[0] = new Ghost();
         ghostArray[1] = new Ghost();
         ghostArray[2] = new Ghost();
         ghostArray[3] = new Ghost();
         ghostArray[4] = new Ghost();
-        this.setPosGhost();
+        for(var i=0;i<ghostArray.length;i++){
+          ghostArray[i].setPosition(new cc.Point(0,550));
+        }
         this.createGhost();
-        this.scheduleUpdate();
+
+
         this.scoreLabel = cc.LabelTTF.create( '0', 'Arial', 40 );
 	      this.scoreLabel.setPosition( new cc.Point( 750, 550 ) );
 	      this.addChild( this.scoreLabel );
+
+        this.scheduleUpdate();
+
         return true;
     },
     update : function(){
       this.checkBullet();
       this.createGhost();
+      this.checkGhost();
+    },
+
+    checkGhost: function(){
       for(var i=0;i<ghostArray.length;i++){
         if(ghostArray[i].hit(this.bullet)){
-          this.removeChild(ghostArray[i]);
-          this.removeChild(this.bullet);
-          this.bullet.setPositionY(700);
           ghostArray[i].setPosition(new cc.Point(0,550));
-          this.checkGradeLevel();
+          this.removeChild(ghostArray[i]);
           score++;
           this.scoreLabel.setString(score);
+          this.removeChild(this.bullet);
+          this.bullet.setPositionY(700);
+          this.checkGradeLevel();
         }
         if(this.checker.gameOver(ghostArray[i])){
-          for(var j=0;j<ghostArray.length;j++){
-          ghostArray[j].unscheduleUpdate();
-          }
+          this.stopGame();
         }
-
       }
-
     },
+
+    stopGame: function(){
+      for(var j=0;j<ghostArray.length;j++){
+      ghostArray[j].unscheduleUpdate();
+      }
+      this.bullet.unscheduleUpdate();
+    },
+
     checkGradeLevel: function(){
 
        if(level == 1){
-         countGhost[0]-=1;
-          if(countGhost[0]==0){
-            level += 1;
-            count=0;
-            this.updateMoveSpeed();
-          }
+         this.runGradeLevel(0);
       }else if(level == 2){
-        countGhost[1]-=1;
-        if(countGhost[1]==0){
-          level += 1;
-          count=0;
-          this.updateMoveSpeed();
-        }
+        this.runGradeLevel(1);
       }else if(level == 3){
-        countGhost[2] -= 1;
-        if(countGhost[2] == 0){
-          level += 1;
-          count=0;
-          this.updateMoveSpeed();
-        }
+        this.runGradeLevel(2);
       }else if(level == 4){
-        countGhost[3] -= 1;
-        if(countGhost[3] == 0){
-          level += 1;
-          count=0;
-          this.updateMoveSpeed();
-        }
+        this.runGradeLevel(3);
       }else {
-        countGhost[4] -= 1;
-        if(countGhost[4] == 0){
-          count=0;
-          level+=1;
-          countGhost[4] = 5 ;
-          this.updateMoveSpeed();
-        }
+        this.runGradeLevel(4);
+      }
+
+    },
+    runGradeLevel: function(index){
+      countGhost[index] -= 1;
+      if(countGhost[index] == 0){
+        count=0;
+        level+=1;
+        this.updateMoveSpeed();
+        if(index == 4)
+        countGhost[index] = 5 ;
       }
     },
+
     updateMoveSpeed: function(){
       for(var i=0;i<ghostArray.length;i++){
         ghostArray[i].velocityUp();
       }
     },
-    setPosGhost: function(){
-      for(var i=0;i<ghostArray.length;i++){
-        ghostArray[i].setPosition(new cc.Point(0,550));
-      }
-    },
+
     createGhost: function(){
 
       count+=1;
+
       if(level == 1){
         if(count==1){
           this.addGhost(0);
@@ -147,6 +149,7 @@ var GameLayer = cc.LayerColor.extend({
       this.addChild(ghostArray[index]);
       ghostArray[index].scheduleUpdate();
     },
+
     onKeyDown: function( keyCode, event ) {
       if(keyCode == cc.KEY.space){
         if(check==true){
@@ -156,15 +159,14 @@ var GameLayer = cc.LayerColor.extend({
        }
        }
     },
+
     checkBullet: function(){
+
       if(this.bullet.getPositionY() > 600 || this.bullet.getPositionY()==0){
         check =true;
       }else{
         check = false;
       }
-
-    },
-    endGame: function(){
 
     },
     onKeyUp: function( keyCode, event ) {
